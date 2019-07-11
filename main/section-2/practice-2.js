ee949af6530c539446d199cb08b4cdf6442b787e
zhanggi3 <gio.zhang@oocl.com>
2019-07-11 13:03:10 +0800

    feat: implement section2 - practice-2

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+ let obj = {};
+     let objItem = [];
+     collection.forEach((element)=>{
+         if(element.indexOf('-')>-1){
+              let array = element.split('-');
+              let key = array[0];
+              let value = parseInt(array[1]);
+              obj[key]?obj[key]+=value:obj[key]=value;
+         }else{
+         obj[element]?obj[element]+=1:obj[element]=1;
+         }
+     });
+
+     let keyArray = Object.keys(obj);
+     let valueArray = Object.values(obj);
+     keyArray.map((value,index)=>{
+         let item ={};
+         item.key=value;
+         item.count=valueArray[index];
+         objItem.push(item);
+     });
+   return objItem;
 }
